305582c3cf562ffd93e35f1bcf935a1bfd5003dd
takuya44 <78689643+takuya44@users.noreply.github.com>
2021-02-13 11:41:34 +0900

    完了と戻す機能

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ export const App = () => {
     "あああ",
     "いいい"
   ]);
+
   const [comopleteTodos, serComoleteTodos] = useState(["ううう"]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
+  //追加ボタン
   const onClickAdd = () => {
     if (todoText === "") return;
     const newTodos = [...incomopleteTodos, todoText];
@@ -19,10 +21,37 @@ export const App = () => {
     setTodoText("");
   };
 
+  //削除ボタン
   const onClickDelete = (index) => {
     const newTodos = [...incomopleteTodos];
+    //console.log(newTodos);
     newTodos.splice(index, 1);
     setIncomopleteTodos(newTodos);
+  };
+
+  //完了ボタン
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incomopleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newcomopleteTodos = [...comopleteTodos, incomopleteTodos[index]];
+    //未完了Todoから削除
+    setIncomopleteTodos(newIncompleteTodos);
+    //完了Todoへ追加
+    serComoleteTodos(newcomopleteTodos);
+  };
+
+  //戻すボタン
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...comopleteTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incomopleteTodos, comopleteTodos[index]];
+
+    //完了Todoから削除
+    serComoleteTodos(newCompleteTodos);
+    //未完了Todoへ追加
+    setIncomopleteTodos(newIncompleteTodos);
   };
 
   return (
@@ -42,7 +71,7 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
@@ -52,11 +81,11 @@ export const App = () => {
       <div className="comoplete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          {comopleteTodos.map((todo) => {
+          {comopleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={() => onClickBack(index)}>戻す</button>
               </div>
             );
           })}
